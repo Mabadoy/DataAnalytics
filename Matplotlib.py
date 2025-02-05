0b06e24d89069e44d9cc2c5cdb380ae6f89842ac
Marc <mabadoy@gmail.com>
2025-02-05 15:21:00 +1300

    Bar Chart

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [10, 20, 25, 30, 40]
-plt.plot(x, y, marker='o')
+x = [1, 2, 3, 4, 1, 5]
+y = [10, 20, 25, 30, 40, 10]
+plt.plot(x, y, marker='o', color='red')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Basic Line Plot')
+plt.show()
+
+categories = ['Cloud Security', 'Data Analytics', 'BlockChain', 'ITS']
+values = [88, 97, 82, 91]
+plt.bar(categories, values, color='skyblue')
+plt.xlabel('Subjects')
+plt.ylabel('Grades')
+plt.title('Yoobee')
 plt.show()
