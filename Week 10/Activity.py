b225ce879ad427b7ae8ec795584d7c9ab1e079a8
Marc <mabadoy@gmail.com>
2025-02-12 16:04:04 +1300

    Seaborn

---
+++ Week 10/Activity.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("Sample_Data_for_Activity.csv")
+print("Print top 5 records:")
+print(df.head())
+
+print("Data Information")
+print(df.info())
+
+sns.displot(df['Normal_Distribution'], kde=True, bins=50)
+sns.jointplot(x='Normal_Distribution', y='Uniform_Distribution', data=df, kind='scatter')
